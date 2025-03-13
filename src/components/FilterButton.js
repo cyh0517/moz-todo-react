aabfcaafce402d54e10f6fbd2d47b301ea27ea3b
cyh <1356613034@qq.com>
2025-03-13 17:09:30 +0800

    add in Todo.js

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,12 +1,41 @@
 import React from "react";
+import { useState } from "react";
 
-function FilterButton(props) {
+export default function Form(props) {
+
+    const [name, setName] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        props.onSubmit(name);
+        setName("");
+    }
+
+    function handleChange(event) {
+        setName(event.target.value);
+    }
+
     return (
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-            <span className="visually-hidden">Show </span>
-            <span>{props.filterName}</span>
-            <span className="visually-hidden"> tasks</span>
-        </button>
+        <form onSubmit={handleSubmit}>
+            <h2 className="label-wrapper">
+                <label htmlFor="new-todo-input" className="label__lg">
+                    What needs to be done
+                </label>
+            </h2>
+            <input
+                type="text"
+                id="new-todo-input"
+                className="input input__lg"
+                name="text"
+                autoComplete="off"
+                value={name}
+                onChange={handleChange}
+            />
+            <button type="submit" className="btn btn__primary btn__lg">
+                Add
+            </button>
+        </form>
     );
 }
-export default FilterButton;
+
+ 
