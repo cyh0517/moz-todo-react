3d2d5edd8c8af2b61410a7a7f23df026725f4ee2
cyh <1356613034@qq.com>
2025-03-13 16:45:04 +0800

    change in App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,72 @@
 import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
 import Todo from "./components/Todo";
-import { useState } from "react";
-import { nanoid } from "nanoid";
+import { useEffect, useRef, useState } from "react";
+import { useTaskStore } from "./store/todo";
 
-export default function App(props) {
-  const [tasks, setTasks] = useState(props.tasks);
-  function addTask(name) {
-    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+export default function App() {
 
-    setTasks([...tasks, newTask]);
-    console.log(tasks);
-  }
+  const { allTasks, fetchAllTasks, addTask, deleteTask, editTask, toggleTaskCompleted } = useTaskStore();
 
-  const taskList = tasks.map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
+  const headRef = useRef(null);
+
+  const [filter, setFilter] = useState("All");
+
+  useEffect(() => {
+    fetchAllTasks();
+  }, [])
+
+  const FILTER_MAP = {
+    All: () => true,
+    Active: (task) => !task.completed,
+    Completed: (task) => task.completed,
+  };
+
+  const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ));
-  console.log(taskList);
+
+  const taskList = allTasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
+      <h1 onClick={() => { console.log(headRef)}}>TodoMatic</h1>
       <Form onSubmit={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton filterName="All" />
-        <FilterButton filterName="Active" />
-        <FilterButton filterName="Completed" />
+        {filterList}
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading" ref={headRef}>{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
         {taskList}
+
       </ul>
     </div>
   );
 }
+
