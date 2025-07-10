961f8870c4ee9271d8e3c66bf7531bc9dc9e766d
cyh <1356613034@qq.com>
2025-07-10 11:57:25 +0800

    finish deleting tasks and sycing completed status

--- src/App.js
+++ src/App.js
@@ -16,10 +16,41 @@ export default function App(props) {
     console.log(tasks);
   }
 
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+}
+
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+    console.log(updatedTasks);
+}
+
+
   const taskList = tasks?.map((task) => (
-  <Todo id = {task.id} name = {task.name} completed = {task.completed} key = {task.id}/>
+  <Todo 
+    id = {task.id}
+    name = {task.name} 
+    completed = {task.completed} 
+    key = {task.id} 
+    toggleTaskCompleted = {toggleTaskCompleted}
+    deleteTask = {deleteTask}
+  />
 ));
-  console.log(taskList);
+  
+  const taskNoun = taskList.length != 1 ? "tasks" : "task";
+  const headingTest = `${taskList.length} ${taskNoun} remaining`;
+  
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -29,7 +60,7 @@ export default function App(props) {
         <FilterButton filterName = "active"/>
         <FilterButton filterName = "completed"/>
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingTest}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
